12d2767382a551487a492047baa09f2ea448bb90
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2022-01-21 17:03:45 +0100

    Add files via upload

---
+++ scripts/injector.js
@@ -0,0 +1,7 @@
+function inject() {
+	Module.import('definers')
+	Module.import('map')
+	Module.import('placer')
+	
+	Module.cssImport('character')
+}
---
+++ scripts/map.js
@@ -0,0 +1,8 @@
+map = [
+	[g,s,g,g,s,g,g,s,g,g],
+	[g,s,s,g,g,g,g,s,g,g],
+	[g,g,g,g,w,g,g,g,s,g],
+	[g,s,g,s,w,w,g,s,s,g],
+	[g,s,g,s,g,s,g,s,s,g],
+	[g,s,g,s,g,s,g,s,s,g],
+]
